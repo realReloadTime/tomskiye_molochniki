7a79afdbc688c6b2c9facb58e9cadb12d0004fb6
VyacheslavBelg <vaceslavcujkov@gmail.com>
2025-11-29 21:41:19 +0700

    для фронта (анализ файла)

--- site_backend/BackendSite/obj/Release/net9.0/BackendSite.AssemblyInfo.cs
+++ site_backend/BackendSite/obj/Release/net9.0/BackendSite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a9ecacbec86bb56d443e1184e2bde77a5b243af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6c666ac0942a9acdda25b14b6f27b4278661417")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
